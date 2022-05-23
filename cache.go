d5da3b0c301cbb1a3dddcd5eee17d05eab7a5a14
Timofey <sherbinin5@gmail.com>
2022-05-23 21:57:53 +0300

    solution for cache task

--- cache.go
+++ cache.go
@@ -3,21 +3,43 @@ package cache
 import "time"
 
 type Cache struct {
+	values map[string]cacheValue
+}
+
+type cacheValue struct {
+	value   string
+	expired time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{map[string]cacheValue{}}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (this Cache) Get(key string) (string, bool) {
+	if v, ok := this.values[key]; ok {
+		if v.expired.IsZero() || v.expired.After(time.Now()) {
+			return v.value, true
+		}
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (this Cache) Put(key, value string) {
+	this.values[key] = cacheValue{value, time.Time{}}
 }
 
-func (receiver) Keys() []string {
+func (this Cache) Keys() []string {
+	result := []string{}
+
+	now := time.Now()
+	for k, v := range this.values {
+		if v.expired.IsZero() || v.expired.After(now) {
+			result = append(result, k)
+		}
+	}
+	return result
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (this Cache) PutTill(key, value string, deadline time.Time) {
+	this.values[key] = cacheValue{value, deadline}
 }
